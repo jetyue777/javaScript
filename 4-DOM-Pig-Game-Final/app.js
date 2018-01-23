bd01ac6e43685308ad8dc71680f9017cfb3a0eee
jetyue <jetyue777@gmail.com>
2018-01-22 20:02:28 -0500

    end dom manipulation

--- 4-DOM-Pig-Game-Final/app.js
+++ 4-DOM-Pig-Game-Final/app.js
@@ -14,34 +14,81 @@ GAME RULES:
 var scores, roundScore, activePlayer, gamePlaying;
 
 //able to execute function due to hoisting (function declaration)
-//creation phase of execution context
+//creation phase of execution context, Variable Object is created
+//code scan through to find function declaration during Variable Object Creation
+//for each function, a property
+//is created in the Variable Object, pointing to the function
 init();
 
+//document object for dom manipulation (object give us access to the dom)
+//querySelect is a useful method to select dom object
+//select things same as CSS, but only select first element it find, but has workaround
+//event listener: a function that performs an action based on a certain event.
+// it waits for a specific event to happen
 
+/* How is event then processed?
+
+    Remember about the execution context. (execution stack)
+    - Event can only be processed or handled when the execution stack is empty
+    - which means all the functions have returned.
+
+    Beside of Execution Stack, we also have "Message Queue" in the javaScript Engine
+    This is where all the event that happen in the browser are put.
+    These events sit here and wait to be processed (after execution stack becomes empty)
+
+    1) finish executing all functions in Execution stack
+    2) Message Queue takes in an event such as "click event"
+    3) put clickHandler() on execution Stack
+
+
+
+* */
+
+//register an event listener to perform an action to a dom element when specific event occurs
+//'click' is one of the many events
+//takes in two parameters (event, call back function)
+
+// we don't want to call the anonymous function here, we want the eventListener to call
+// the function for us, so we put the the function name as call back
+
+// A callback function is a function that we pass into another function as an argument
+// it is not called by us, it's called by another function
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
         // 1. Random number
+        //Math.random() return a random number between 0 and 1
+        //Math.floor() removes the decimal portion of a number
         var dice = Math.floor(Math.random() * 6) + 1;
 
         //2. Display the result
+        //querySelector only selects the first one that matches
+        /*
+         <img src="dice-5.png" alt="Dice" class="dice" id="dice-1">
+         <img src="dice-5.png" alt="Dice" class="dice" id="dice-2">
+        * */
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
+        //change the src attribute
         diceDOM.src = 'dice-' + dice + '.png';
 
 
         //3. Update the round score IF the rolled number was NOT a 1
+        // !== does not do type coercion
         if (dice !== 1) {
             //Add score
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
             //Next player
+            //Use function here to don't repeat yourself (DRY principle)
             nextPlayer();
         }
     }    
 });
 
-
+/*Remember about the execution context. (execution stack)
+- Event can only be processed or handled when the execution stack is empty
+- which means all the functions have returned.*/
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
         // Add CURRENT score to GLOBAL score
@@ -54,11 +101,14 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
+
+            //should add or remove CSS class in javaScript instead of changing CSS properties all the time
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
         } else {
             //Next player
+            //Use function here to don't repeat yourself (DRY principle)
             nextPlayer();
         }
     }
@@ -67,6 +117,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 function nextPlayer() {
     //Next player
+
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
@@ -92,6 +143,8 @@ function init() {
     
     document.querySelector('.dice').style.display = 'none';
 
+    //same as
+    //document.querySelector('#score-0').textContent = '0';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
@@ -107,6 +160,7 @@ function init() {
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+//get value from dom
 //var x = document.querySelector('#score-0').textContent;
 
 
